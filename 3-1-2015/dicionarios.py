c673d0dfc495ffe20795a582cb30367a0bf150ab
p1xies <cleyton@tagi.com.br>
2015-01-03 17:15:51 -0300

    Refatorado

--- 3-1-2015/dicionarios.py
+++ 3-1-2015/dicionarios.py
@@ -1,9 +1,10 @@
 # -*- coding;UTF-8 -*-
-Renato = { 'Marguerita' : 4, 'Quatro_queijos' : 5, 'Escarola' : 4, 'Portuguesa' : 5, "nome": "Renato" }
-Marcelo = { 'Marguerita' : 2, 'Quatro_queijos' : 2, 'Escarola' : 1, 'Portuguesa' : 3, "nome": "Marcelo"}
-Lenon = { 'Marguerita' : 4, 'Quatro_queijos' : 5, 'Escarola' : 2, 'Portuguesa' : 1, "nome": "Lenon" }
-Renata = { 'Marguerita' : 4, 'Quatro_queijos' : 5, 'Escarola' : 1, 'Portuguesa' : 1, "nome": "Renata"}
-Washington = { 'Marguerita' : 1, 'Quatro_queijos' : 1, 'Escarola' : 2, 'Portuguesa' : 3, "nome": "Washington" }
-Tino = { 'Marguerita' : 1, 'Quatro_queijos' : 5, 'Escarola' : 1, 'Portuguesa' : 4,  "nome": "Tino" }
-Luca = { 'Marguerita' : 5, 'Quatro_queijos' : 4, 'Escarola' : 3, 'Portuguesa' : 4, "nome": "Luca"}
-
+pessoas = [
+	{ 'Marguerita' : 4, 'Quatro_queijos' : 5, 'Escarola' : 4, 'Portuguesa' : 5, "nome": "Renato" },
+	{ 'Marguerita' : 2, 'Quatro_queijos' : 2, 'Escarola' : 1, 'Portuguesa' : 3, "nome": "Marcelo"},
+	{ 'Marguerita' : 4, 'Quatro_queijos' : 5, 'Escarola' : 2, 'Portuguesa' : 1, "nome": "Lenon" },
+	{ 'Marguerita' : 4, 'Quatro_queijos' : 5, 'Escarola' : 1, 'Portuguesa' : 1, "nome": "Renata"} ,
+	{ 'Marguerita' : 1, 'Quatro_queijos' : 1, 'Escarola' : 2, 'Portuguesa' : 3, "nome": "Washington" },
+	{ 'Marguerita' : 1, 'Quatro_queijos' : 5, 'Escarola' : 1, 'Portuguesa' : 4,  "nome": "Tino" },
+	{ 'Marguerita' : 5, 'Quatro_queijos' : 4, 'Escarola' : 3, 'Portuguesa' : 4, "nome": "Luca"}
+	]
--- 3-1-2015/pizza.py
+++ 3-1-2015/pizza.py
@@ -1,10 +1,12 @@
 # -*- coding;UTF-8 -*-
 from dicionarios import *
+
+
 def inferir(nome_pizza):
-	
-	pessoas = [Renato, Lenon, Renata, Luca, Marcelo, Washington, Tino]
-	result = []
-	for pessoa in pessoas: 
-		if pessoa[nome_pizza]>=3:
-			result.append(pessoa["nome"])
-	return result
+
+    pessoas = [Renato, Lenon, Renata, Luca, Marcelo, Washington, Tino]
+    result = []
+    for pessoa in pessoas:
+        if pessoa[nome_pizza] >= 3:
+            result.append(pessoa["nome"])
+    return result
