1833e9b8c0955bbae281d64eee3d45db3db863a7
p1xies <cleyton@tagi.com.br>
2015-01-03 16:34:58 -0300

    modificação de Anderson e Vinicius

--- 3-1-2015/pizza.py
+++ 3-1-2015/pizza.py
@@ -1,4 +1,10 @@
 from dicionarios import *
 def inferir(nome_pizza):
-	if nome_pizza == 'Marguerita':
-		return [Renato,Lenon,Renata,Luca]
+	#if nome_pizza == 'Marguerita':
+		#return [Renato,Lenon,Renata,Luca]
+	pessoas = [Renato, Lenon, Renata, Luca, Marcelo, Washington, Tino]
+	result = []
+	for pessoa in pessoas: 
+		if pessoa[nome_pizza]>=3:
+			result.append(pessoa)
+	return result
