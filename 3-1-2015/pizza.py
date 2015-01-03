768f9886a6c1b996b8a173f8e3c35d58214a58a6
p1xies <cleyton@tagi.com.br>
2015-01-03 16:21:20 -0300

    modficaçao de nanow

---
+++ 3-1-2015/pizza.py
@@ -0,0 +1,4 @@
+from dicionarios import *
+def inferir(nome_pizza):
+	if nome_pizza == 'Marguerita':
+		return [Renato,Lenon,Renata,Luca]
--- 3-1-2015/test_pizza.py
+++ 3-1-2015/test_pizza.py
@@ -1,4 +1,6 @@
-import unittest
+import unittest 
+from pizza import inferir
+from dicionarios import *
 
 class Test_pizza(unittest.TestCase):
 
@@ -6,5 +8,8 @@ class Test_pizza(unittest.TestCase):
 		self.assertEqual(inferir('Marguerita'),[Renato, Lenon, Renata, Luca])	
 
 
+
+if __name__ == "__main__":
+	unittest.main()
 
 
