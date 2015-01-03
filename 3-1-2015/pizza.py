c0f5585ea11263731998295030316b36655d1afa
p1xies <cleyton@tagi.com.br>
2015-01-03 16:51:21 -0300

    Vitão bagunçou o código

--- 3-1-2015/pizza.py
+++ 3-1-2015/pizza.py
@@ -1,3 +1,4 @@
+# -*- coding;UTF-8 -*-
 from dicionarios import *
 def inferir(nome_pizza):
 	#if nome_pizza == 'Marguerita':
--- 3-1-2015/test_pizza.py
+++ 3-1-2015/test_pizza.py
@@ -1,3 +1,4 @@
+# -*- coding;UTF-8 -*-
 import unittest 
 from pizza import inferir
 from dicionarios import *
@@ -7,7 +8,8 @@ class Test_pizza(unittest.TestCase):
 	def test_se_pizza_margerita(self):
 		self.assertEqual(inferir('Marguerita'),[Renato, Lenon, Renata, Luca])	
 
-
+	def test_se_pizza_4queijos(self):
+		self.assertEqual(inferir('Quatro_queijos'),[Renato, Lenon, Renata, Tino, Luca])
 
 if __name__ == "__main__":
 	unittest.main()
